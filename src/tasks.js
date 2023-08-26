9e3ded4a4f33b4dbe7a2df131ed782e678dee51a
Ivonne <ivonnebenites2015@gmail.com>
2023-08-26 14:19:10 -0500

    Chore: modify tasks.js file

--- src/tasks.js
+++ src/tasks.js
@@ -1,13 +1,17 @@
+import { createEllipsisIcon } from "./print";
+
 export const tasks = [
   {
     description: 'Go to the market',
     completed: false,
-    index: 0,
+    index: 0
   },
 ];
 
 export function createTaskListItem(task) {
   const listItem = document.createElement('li');
+  listItem.classList = 'solo-task';
+
   const checkBox = document.createElement('input');
   checkBox.setAttribute('type', 'checkBox');
   checkBox.classList = 'complete';
@@ -16,8 +20,12 @@ export function createTaskListItem(task) {
   const description = document.createElement('span');
   description.textContent = task.description;
 
+  const ellipsisIcon = createEllipsisIcon();
+
+
   listItem.appendChild(checkBox);
   listItem.appendChild(description);
+  listItem.appendChild(ellipsisIcon);
 
   return listItem;
 }
